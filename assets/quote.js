919ac3a44d4448ec82c7c2126f0053067dcf4b42
Noah <mounderfod@gmail.com>
2023-08-04 20:06:33 +0000

    Add quotes code

---
+++ assets/quote.js
@@ -0,0 +1,15 @@
+bar = document.getElementById("topBar")
+quotes = [
+    "My best friend's a butcher, he has sixteen knives",
+    "I got soul, but I'm not a soldier",
+    "It's different now that I'm poor and aging",
+    "It's High Green mate, via Hillsborough please",
+    "The room is on fire while she's fixing her hair",
+    "I'm going to Wichita, far from this opera forever more",
+    "Coming out of my cage and I've been feeling just fine",
+    "I bet that you look good on the dancefloor",
+    "Oh baby I feel so down"
+]
+
+let random = Math.floor(Math.random() * quotes.length);
+bar.innerText = quotes[random];
