4511e973c158f6ed6a8f8c8466687c4451db4831
mounderfod <mounderfod@gmail.com>
2023-09-23 16:33:01 +0100

    Some fixes

--- assets/quote.js
+++ assets/quote.js
@@ -1,17 +1,17 @@
-let bar = document.getElementById("topBar")
+let bar = document.getElementById("topBar");
 quotes = [
-    "My best friend's a butcher, he has sixteen knives",
-    "I got soul, but I'm not a soldier",
-    "It's different now that I'm poor and aging",
-    "It's High Green mate, via Hillsborough please",
-    "The room is on fire while she's fixing her hair",
-    "I'm going to Wichita, far from this opera forever more",
-    "Coming out of my cage and I've been feeling just fine",
-    "I bet that you look good on the dancefloor",
-    "Oh baby I feel so down",
-    "I'm just trying to find a mountain I can climb",
-    "Outside the cafby the cracker factory",
-]
+  "My best friend's a butcher, he has sixteen knives",
+  "I got soul, but I'm not a soldier",
+  "It's different now that I'm poor and aging",
+  "It's High Green mate, via Hillsborough please",
+  "The room is on fire while she's fixing her hair",
+  "I'm going to Wichita, far from this opera forever more",
+  "Coming out of my cage and I've been feeling just fine",
+  "I bet that you look good on the dancefloor",
+  "Oh baby I feel so down",
+  "I'm just trying to find a mountain I can climb",
+  "Outside the café by the cracker factory",
+];
 
 let random = Math.floor(Math.random() * quotes.length);
 bar.innerText = quotes[random];
